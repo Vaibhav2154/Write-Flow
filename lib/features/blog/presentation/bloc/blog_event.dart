7d9d6efbe568972ce38a83b8511556a211faaa5d
Vaibhav2154 <vaibhavvaibhu2005@gmail.com>
2025-03-18 15:14:54 +0530

    Blog presentation layer

---
+++ lib/features/blog/presentation/bloc/blog_event.dart
@@ -0,0 +1,21 @@
+part of 'blog.dart';
+
+@immutable
+sealed class BlogEvent {}
+
+
+final class BlogUpload extends BlogEvent{
+  final String title;
+  final String posterId;
+  final String content;
+  final File image;
+  final List<String> topics;
+
+  BlogUpload({
+    required this.title,
+    required this.posterId,
+    required this.content,
+    required this.image,
+    required this.topics,
+  });
+}
