da04d9262761a87ac9a199adde8c40e7e8ed5493
Vaibhav2154 <vaibhavvaibhu2005@gmail.com>
2025-03-18 16:31:57 +0530

    Upload blogs done

--- lib/features/blog/presentation/pages/add_new_blog_page.dart
+++ lib/features/blog/presentation/pages/add_new_blog_page.dart
@@ -1,10 +1,16 @@
 import 'dart:io';
+import 'package:blog_app/core/common/cubits/app_user/app_user_cubit.dart';
+import 'package:blog_app/core/common/widgets/loader.dart';
 import 'package:blog_app/core/theme/app_pallete.dart';
 import 'package:blog_app/core/utils/pick_image.dart';
+import 'package:blog_app/core/utils/show_snacbac.dart';
+import 'package:blog_app/features/blog/presentation/bloc/blog.dart';
+import 'package:blog_app/features/blog/presentation/pages/blog_page.dart';
 import 'package:blog_app/features/blog/presentation/widgets/blog_editor.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AddNewBlogPage extends StatefulWidget {
   static route() =>
@@ -19,6 +25,7 @@ class _AddNewBlogPageState extends State<AddNewBlogPage> {
   final titleController = TextEditingController();
   final contentController = TextEditingController();
   List<String> selectedTopics = [];
+  final formKey = GlobalKey<FormState>();
   File? image;
 
   void selectImage() async {
@@ -27,6 +34,23 @@ class _AddNewBlogPageState extends State<AddNewBlogPage> {
       setState(() {
         image = pickedImage;
       });
+    }
+  }
+
+  void uploadBlog() {
+    if (formKey.currentState!.validate() &&
+        selectedTopics.isNotEmpty &&
+        image != null) {
+      // Add Blog
+      final posterId =
+          (context.read<AppUserCubit>().state as AppUserLoggedIn).user.id;
+      context.read<BlogBloc>().add(BlogUpload(
+            title: titleController.text.trim(),
+            posterId: posterId,
+            content: contentController.text.trim(),
+            image: image!,
+            topics: selectedTopics,
+          ));
     }
   }
 
@@ -43,134 +67,161 @@ class _AddNewBlogPageState extends State<AddNewBlogPage> {
       appBar: AppBar(
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: uploadBlog,
             icon: Icon(Icons.done_rounded),
           ),
         ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 40,
-              ),
-              image !=null ?
-              GestureDetector(
-                onTap: () {
-                  selectImage();
-                },
-                child: SizedBox(
-                  height: 200,
-                  width: double.infinity,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.file(
-                      image!,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-              ) :
-              GestureDetector(
-                onTap: () {
-                  selectImage();
-                },
-                child: DottedBorder(
-                  color: AppPallete.greyColor,
-                  dashPattern: [
-                    10,
-                    4,
-                  ],
-                  strokeWidth: 2,
-                  strokeCap: StrokeCap.round,
-                  borderType: BorderType.RRect,
-                  radius: Radius.circular(10),
-                  child: Container(
-                      height: 150,
-                      width: double.infinity,
-                      child: image != null
-                          ? Image.file(
-                              image!,
-                              fit: BoxFit.cover,
-                            )
-                          : Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                const Icon(
-                                  CupertinoIcons.folder,
-                                  size: 40,
-                                  // color: AppPallete.primaryColor,
-                                ),
-                                SizedBox(
-                                  height: 15,
-                                ),
-                                const Text(
-                                  'Select your Image',
-                                  style: TextStyle(fontSize: 15),
-                                ),
-                              ],
-                            )),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
+        child: BlocConsumer<BlogBloc, BlogState>(
+          listener: (context,state){
+          if(state is BlogFailure){
+            showSnackBar(context, state.error);
+          }if(state is BlogSuccess){
+            Navigator.pushAndRemoveUntil(
+              context, 
+              BlogPage.route(),
+              (route) => false,
+            );
+          }
+          },
+          builder: (context, state) {
+            if(state is BlogLoading){
+              return Loader();
+            }
+            return SingleChildScrollView(
+              child: Form(
+                key: formKey,
+                child: Column(
                   children: [
-                    'Technology',
-                    'Business',
-                    'Programming',
-                    'Entertainment',
-                  ]
-                      .map(
-                        (e) => Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: GestureDetector(
+                    SizedBox(
+                      height: 40,
+                    ),
+                    image != null
+                        ? GestureDetector(
                             onTap: () {
-                              if (selectedTopics.contains(e)) {
-                                selectedTopics.remove(e);
-                              } else {
-                                selectedTopics.add(e);
-                              }
-                              setState(() {});
+                              selectImage();
                             },
-                            child: Chip(
-                              label: Text(e),
-                              color: selectedTopics.contains(e)
-                                  ? MaterialStatePropertyAll(
-                                      AppPallete.gradient2)
-                                  : null,
-                              labelStyle: TextStyle(
-                                fontSize: 18,
+                            child: SizedBox(
+                              height: 200,
+                              width: double.infinity,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image.file(
+                                  image!,
+                                  fit: BoxFit.cover,
+                                ),
                               ),
-                              side: selectedTopics.contains(e)
-                                  ? null
-                                  : BorderSide(
-                                      color: AppPallete.greyColor,
-                                      width: 1,
-                                    ),
+                            ),
+                          )
+                        : GestureDetector(
+                            onTap: () {
+                              selectImage();
+                            },
+                            child: DottedBorder(
+                              color: AppPallete.greyColor,
+                              dashPattern: [
+                                10,
+                                4,
+                              ],
+                              strokeWidth: 2,
+                              strokeCap: StrokeCap.round,
+                              borderType: BorderType.RRect,
+                              radius: Radius.circular(10),
+                              child: Container(
+                                  height: 150,
+                                  width: double.infinity,
+                                  child: image != null
+                                      ? Image.file(
+                                          image!,
+                                          fit: BoxFit.cover,
+                                        )
+                                      : Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            const Icon(
+                                              CupertinoIcons.folder,
+                                              size: 40,
+                                              // color: AppPallete.primaryColor,
+                                            ),
+                                            SizedBox(
+                                              height: 15,
+                                            ),
+                                            const Text(
+                                              'Select your Image',
+                                              style: TextStyle(fontSize: 15),
+                                            ),
+                                          ],
+                                        )),
                             ),
                           ),
-                        ),
-                      )
-                      .toList(),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          'Technology',
+                          'Business',
+                          'Programming',
+                          'Entertainment',
+                        ]
+                            .map(
+                              (e) => Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: GestureDetector(
+                                  onTap: () {
+                                    if (selectedTopics.contains(e)) {
+                                      selectedTopics.remove(e);
+                                    } else {
+                                      selectedTopics.add(e);
+                                    }
+                                    setState(() {});
+                                  },
+                                  child: Chip(
+                                    label: Text(e),
+                                    color: selectedTopics.contains(e)
+                                        ? MaterialStatePropertyAll(
+                                            AppPallete.gradient2)
+                                        : null,
+                                    labelStyle: TextStyle(
+                                      fontSize: 18,
+                                    ),
+                                    side: selectedTopics.contains(e)
+                                        ? null
+                                        : BorderSide(
+                                            color: AppPallete.greyColor,
+                                            width: 1,
+                                          ),
+                                  ),
+                                ),
+                              ),
+                            )
+                            .toList(),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    BlogEditor(
+                      controller: titleController,
+                      hintText: 'Blog Title',
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    BlogEditor(
+                      controller: contentController,
+                      hintText: 'Blog Content',
+                    ),
+                  ],
                 ),
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              BlogEditor(controller: titleController, hintText: 'Blog Title'),
-              const SizedBox(
-                height: 20,
-              ),
-              BlogEditor(
-                  controller: contentController, hintText: 'Blog Content'),
-            ],
-          ),
+            );
+          },
         ),
       ),
     );
